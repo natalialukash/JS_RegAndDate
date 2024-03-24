b863e71cbba1205f415ff2faee58a4c13731d269
natalialukash <lukashova.n13@gmail.com>
2024-03-24 14:18:45 +0300

    Слияние

--- js/8.1.js
+++ js/8.1.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 let day1=prompt("Введите дату в формате гг-мм-дд")
 //re=/^((19[0-9])|(20[0-2][0-4]))-((0[1-9])|(1[0-2]))-((0[1-9])|([1-2][0-9])|(3[01]))$/;
 //if (re.test(day1)) alert (getDayOfWeek(numDay))
 //else alert("Дата введена некорректно")  
-=======
-let day1=prompt("Введите дату в формате гг,мм,дд")
-//здесь должна быть регулярка на проверку ввода даты
->>>>>>> 12ec2e08db37f83b4579d1c491d75d4d694d941a
 let isDay=(day2)=>day2=new Date(day1);
 let numDay=isDay()
 function getDayOfWeek(numDay){
@@ -19,11 +14,4 @@ dayWeek=['Воскресенье',
 'Суббота'];
 num=numDay.getDay();
 return dayWeek[num];}
-<<<<<<< HEAD
 alert (getDayOfWeek(numDay))
-=======
-alert(getDayOfWeek(numDay))
->>>>>>> 12ec2e08db37f83b4579d1c491d75d4d694d941a
-
-
-
--- js/8.2.js
+++ js/8.2.js
@@ -1,17 +1,7 @@
 function dayNewYear(){
-<<<<<<< HEAD
-    let dataToday=new Date();
-    let dataNewYear=new Date(dataToday.getFullYear(),0,1);
-    dataNewYear.setFullYear(dataToday.getFullYear()+1)
-    let diff=((dataNewYear-dataToday)/(1000*60*60*24)).toFixed(0)
-    return diff}
-    alert(dayNewYear())
-=======
 let dataToday=new Date();
 let dataNewYear=new Date(dataToday.getFullYear(),0,1);
 dataNewYear.setFullYear(dataToday.getFullYear()+1)
 let diff=((dataNewYear-dataToday)/(1000*60*60*24)).toFixed(0)
 return diff}
 alert(dayNewYear())
-
->>>>>>> 12ec2e08db37f83b4579d1c491d75d4d694d941a
