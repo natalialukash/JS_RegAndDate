66e3a0bea4c6635f82dd877d78a2a1feab4beb77
natalialukash <lukashova.n13@gmail.com>
2024-03-24 15:52:27 +0300

    Задача 8.5 скорректирована

--- js/8.5.js
+++ js/8.5.js
@@ -1,4 +1,4 @@
 let DR=prompt("Введите свою дату рождения");
 let dataBirthday=new Date(DR)
 let dataToday=new Date()
-console.log(((dataToday-dataBirthday)/(1000*60*60*24*365)).toFixed(0))
+console.log(`Вы живете на этом свете ${((dataToday-dataBirthday)/(1000*60*60*24*365)).toFixed(0)} лет, ${12-(dataBirthday.getMonth()-dataToday.getMonth())} месяцев и ${(dataToday.getDate()-dataBirthday.getDate())} дней`)
