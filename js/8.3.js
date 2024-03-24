bb6584c4d45fc2d28bf7072501ab1d4851c729e9
natalialukash <lukashova.n13@gmail.com>
2024-03-24 13:50:33 +0300

    Задача 8.3 скорректирована

--- js/8.3.js
+++ js/8.3.js
@@ -1,17 +1,52 @@
-let day1=prompt("Введите дату в формате гг-мм-дд") 
-let isDay=(day2)=>day2=new Date(day1);
-let numDay=isDay()
-function getDayOfWeek(numDay){
-dayWeek=['Воскресенье',
-'Понедельник',
-'Вторник',
-'Среда',
-'Четверг',
-'Пятница',
-'Суббота'];
-num=numDay.getDay();
-return `${dayWeek[num-1]}, ${dayWeek[num]}, ${dayWeek[num+1]}`;}
-alert (getDayOfWeek(numDay))
+let day1=prompt("Введите дату в формате гг-мм-дд")
+let day2=new Date(day1)
+day3=day2.getDay()
+function IsThreeDay(){
+let dataObj0={
+'prev': "cб",
+'curr': "вс",
+'next': "пн"
+}
+let dataObj1={
+'prev': "вс",
+'curr': "пн",
+'next': "вт"
+}
+let dataObj2={
+'prev': "пн",
+'curr': "вт",
+'next': "ср"
+}
+let dataObj3={
+	'prev': "вт",
+	'curr': "ср",
+	'next': "чт"
+	}
+let dataObj4={
+	'prev': "ср",
+	'curr': "чт",
+	'next': "пт"
+	}
+let dataObj5={
+	'prev': "чт",
+	'curr': "пт",
+	'next': "сб"
+	}
+let dataObj6={
+	'prev': "пт",
+	'curr': "сб",
+	'next': "вс"
+	}
+
+if (day3=="0") return dataObj0
+else if (day3=="1") return dataObj1
+else if (day3=="2") return dataObj2
+else if (day3=="3") return dataObj3
+else if (day3=="4") return dataObj4
+else if (day3=="5") return dataObj5
+else if (day3=="6") return dataObj6
+}
+console.log(IsThreeDay())
 
 
 
