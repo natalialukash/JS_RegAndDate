866fad91c7fae99b3f6bac2e2b1fb335b4aa8cd2
natalialukash <lukashova.n13@gmail.com>
2024-03-24 09:50:07 +0300

    Задача 8.2 решена

---
+++ js/8.2.js
@@ -0,0 +1,8 @@
+function dayNewYear(){
+let dataToday=new Date();
+let dataNewYear=new Date(dataToday.getFullYear(),0,1);
+dataNewYear.setFullYear(dataToday.getFullYear()+1)
+let diff=((dataNewYear-dataToday)/(1000*60*60*24)).toFixed(0)
+return diff}
+alert(dayNewYear())
+
